f6febed11d7cf006aa7efaf96deb8ad887187dca
BugHunter-X <askzenai@gmail.com>
2024-12-08 18:06:06 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,21 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route will never match because it's a child of the /about route */}
+        <Route path="/about/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() { return <h1>Home</h1>; }
+function About() { return <h1>About</h1>; }
+function Contact() { return <h1>Contact</h1>; }
+export default App;
+```
